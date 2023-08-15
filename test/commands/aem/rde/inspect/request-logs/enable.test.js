c8c43186c019f58e2c32c6dbda7545bc28e5e986
Tatyana Vogel <tvogel@adobe.com>
2023-08-15 17:33:40 +0200

    Add enble request log test

--- test/commands/aem/rde/inspect/request-logs/enable.test.js
+++ test/commands/aem/rde/inspect/request-logs/enable.test.js
@@ -36,11 +36,65 @@ describe('EnableRequestLogsCommand', function () {
   setupLogCapturing(sinon, cli);
 
   describe('#check if arguments get passed right', function () {
+    const arg = 'test';
+    const format =
+      '%d{dd.MM.yyyy HH:mm:ss.SSS} *%level* [%thread] %logger %msg%n';
+    const includePathPatterns = '*test';
     const [command, cloudSdkApiStub] = createCloudSdkAPIStub(
       sinon,
-      new EnableRequestLogsCommand([], null),
+      new EnableRequestLogsCommand(
+        [
+          '-i',
+          arg,
+          '-d',
+          arg,
+          '-f',
+          format,
+          '-e',
+          arg,
+          '-p',
+          includePathPatterns,
+          '-i',
+          arg,
+          '-w',
+          arg,
+          '-p',
+          includePathPatterns,
+        ],
+        null
+      ),
       stubbedMethods
     );
+
+    it('should pass the includePathPatterns right', async function () {
+      await command.run();
+      assert.deepStrictEqual(
+        cloudSdkApiStub.enableRequestLogs.args[0][1].includePathPatterns,
+        ['*test', '*test']
+      );
+    });
+
+    it('should formats the format-args right', async function () {
+      await command.run();
+      assert.equal(
+        cloudSdkApiStub.enableRequestLogs.args[0][1].format,
+        '%d{dd.MM.yyyy HH:mm:ss.SSS} *%level* [%thread] %logger %msg%n'
+      );
+    });
+
+    it('should formats the level/logger args right', async function () {
+      await command.run();
+      assert.deepStrictEqual(
+        cloudSdkApiStub.enableRequestLogs.args[0][1].names,
+        [
+          { level: 'INFO', logger: arg },
+          { level: 'INFO', logger: arg },
+          { level: 'DEBUG', logger: arg },
+          { level: 'WARN', logger: arg },
+          { level: 'ERROR', logger: arg },
+        ]
+      );
+    });
 
     it('should call enableRequestLogs() exactly once', async function () {
       await command.run();
