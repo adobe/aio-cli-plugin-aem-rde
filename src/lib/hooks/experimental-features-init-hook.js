8161a332008c77b3b71fb898a9b3117cbb6274f3
Julian Sedding <cod18017@adobe.com>
2023-10-16 12:59:44 +0200

    experimental-features-hook - add support for experimental flags

--- src/lib/hooks/experimental-features-init-hook.js
+++ src/lib/hooks/experimental-features-init-hook.js
@@ -1,15 +1,44 @@
 const Config = require('@adobe/aio-lib-core-config');
 
+let update = function(type, key) {
+  return function (hiddenFeatures) {
+    let filtered = this.config[type].filter(
+        (e) => !hiddenFeatures.find((h) => e[key].startsWith(h))
+    );
+    const removedCount = this.config[type].length - filtered.length;
+    if (removedCount !== 0) {
+      this.config[`_${type}`] = filtered.reduce((acc, e) => {
+        acc.set(e[key], e);
+        return acc;
+      }, new Map());
+      return true;
+    }
+    return false;
+  };
+};
+
 const descriptors = {
   topics: {
-    key: 'name',
+    update: update('topics', 'name'),
   },
   commands: {
-    key: 'id',
-    afterUpdate: function () {
-      this.config._commandIDs = this.config.commands.map((c) => c.id);
-    },
+    update: function(hiddenFeatures) {
+      if (update('commands', 'id').call(this, hiddenFeatures)) {
+        this.config._commandIDs = this.config.commands.map((c) => c.id);
+      }
+    }
   },
+  flags: {
+    update: function(hiddenFeatures) {
+      for (let i = 0; i < hiddenFeatures.length; i++) {
+        let [commandId, flag] = hiddenFeatures[i].split('#');
+        let command = this.config['_commands'].get(commandId);
+        if (command && flag) {
+          delete command.flags[flag];
+        }
+      }
+    },
+  }
 };
 
 /**
@@ -18,22 +47,8 @@ const descriptors = {
  */
 function toggleExperimentalFeatures(type, hiddenFeatures) {
   const descriptor = descriptors[type];
-  const key = descriptor.key;
-  if (hiddenFeatures.length > 0) {
-    const filtered = this.config[type].filter(
-      (e) => !hiddenFeatures.find((h) => e[key].startsWith(h))
-    );
-    const removedCount = this.config[type].length - filtered.length;
-    if (removedCount !== 0) {
-      this.config[`_${type}`] = filtered.reduce((acc, e) => {
-        acc.set(e[key], e);
-        return acc;
-      }, new Map());
-
-      if (typeof descriptor.afterUpdate === 'function') {
-        descriptor.afterUpdate.call(this);
-      }
-    }
+  if (hiddenFeatures.length > 0 && descriptor) {
+    descriptor.update.call(this, hiddenFeatures);
   }
 }
 
@@ -48,5 +63,6 @@ module.exports = async function () {
     const hidden = experimentalFeatures.filter((e) => !enabled.includes(e));
     toggleExperimentalFeatures.call(this, 'topics', hidden);
     toggleExperimentalFeatures.call(this, 'commands', hidden);
+    toggleExperimentalFeatures.call(this, 'flags', hidden);
   }
 };
