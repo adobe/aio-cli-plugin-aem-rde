00f8354284d544555e8aa2ef7d2efd9a8bc61c08
Maria Moalfa <moalfamaria@yahoo.co.uk>
2023-11-13 18:12:26 +0200

    Update install.test.js

--- test/commands/aem/rde/install.test.js
+++ test/commands/aem/rde/install.test.js
@@ -3,10 +3,35 @@ const sinon = require('sinon').createSandbox();
 const Archiver = require('archiver');
 const { archiveDirectory, addDirectoryToArchive } = require('../../../../src/commands/aem/rde/install.js'); 
 const { cli } = require('../../../../src/lib/base-command');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 describe('Archive Utility', function () {
+  let sourceDir;
+  let outputFilePath;
+
+  before(async function () {
+    // Create temporary directories
+    sourceDir = './test-directory';
+    outputFilePath = './test-output.zip';
+    await fs.mkdir(sourceDir);
+  });
+
+  after(async function () {
+    // Check if the file exists before unlinking
+    try {
+      await fs.access(outputFilePath);
+      await fs.unlink(outputFilePath);
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+
+    // Delete temporary directories
+    await fs.rmdir(sourceDir, { recursive: true });
+  });
+
   describe('#archiveDirectory', function () {
     it('should call archiver.finalize() and log the finishing message', async function () {
       const archiverStub = {
@@ -14,11 +39,8 @@ describe('Archive Utility', function () {
         finalize: sinon.stub().resolves(),
       };
       const createWriteStreamStub = sinon.stub(fs, 'createWriteStream').returns({});
-      sinon.stub(Archiver, 'create').returns(archiverStub);
+      sinon.stub(Archiver, 'create').withArgs('zip').returns(archiverStub);
       sinon.stub(cli, 'log');
-
-      const sourceDir = './test-directory';
-      const outputFilePath = './test-output.zip';
 
       await archiveDirectory(sourceDir, outputFilePath);
 
@@ -41,28 +63,28 @@ describe('Archive Utility', function () {
         pipe: sinon.stub(),
       };
 
-      const readdirSyncStub = sinon.stub(fs, 'readdirSync').returns(['file1.txt', 'dir1']);
-      const lstatSyncStub = sinon.stub(fs, 'lstatSync');
-      lstatSyncStub.onCall(0).returns({ isDirectory: () => false, isSymbolicLink: () => false });
-      lstatSyncStub.onCall(1).returns({ isDirectory: () => true });
+      const readdirStub = sinon.stub(fs, 'readdir').resolves(['file1.txt', 'dir1']);
+      const lstatStub = sinon.stub(fs, 'lstat');
+      lstatStub.withArgs(path.join(sourceDir, 'file1.txt')).resolves({ isDirectory: () => false, isSymbolicLink: () => false });
+      lstatStub.withArgs(path.join(sourceDir, 'dir1')).resolves({ isDirectory: () => true });
 
       const joinStub = sinon.stub(path, 'join');
-      joinStub.withArgs('./test-directory', 'file1.txt').returns('file1Path');
+      joinStub.withArgs(sourceDir, 'file1.txt').returns('file1Path');
       joinStub.withArgs('', 'file1.txt').returns('file1ArchivePath');
-      joinStub.withArgs('./test-directory', 'dir1').returns('dir1Path');
+      joinStub.withArgs(sourceDir, 'dir1').returns('dir1Path');
       joinStub.withArgs('dir1Path', 'file2.txt').returns('dir1File2Path');
       joinStub.withArgs('dir1Path', 'subdir').returns('dir1SubdirPath');
 
-      const archiver = Archiver.create();
-      addDirectoryToArchive(archiverStub, './test-directory', '');
+      const archiver = Archiver.create('zip');
+      addDirectoryToArchive(archiverStub, sourceDir, '');
 
       assert.ok(archiverStub.file.calledWith('file1Path', { name: 'file1ArchivePath' }));
       assert.ok(archiverStub.file.calledWith('dir1Path', { name: 'dir1Path' }));
       assert.ok(archiverStub.file.calledWith('dir1File2Path', { name: 'dir1File2Path' }));
       assert.ok(archiverStub.file.calledWith('dir1SubdirPath', { name: 'dir1SubdirPath' }));
 
-      readdirSyncStub.restore();
-      lstatSyncStub.restore();
+      readdirStub.restore();
+      lstatStub.restore();
       joinStub.restore();
     });
   });
