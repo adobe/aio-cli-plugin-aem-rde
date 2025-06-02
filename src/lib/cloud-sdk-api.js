5fe44436e74107ca3452b9f76342deac78067158
rliechti <rliechti@adobe.com>
2025-06-02 11:57:25 +0200

    lint

--- src/lib/cloud-sdk-api.js
+++ src/lib/cloud-sdk-api.js
@@ -395,7 +395,7 @@ class CloudSdkAPI {
     let errMessage = response.statusText;
     try {
       errMessage = await response.text();
-    } catch (err) { }
+    } catch (err) {}
 
     if (errMessage) {
       switch (errMessage) {
